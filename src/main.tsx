c3a0b4988fdc688d6d89b318347ab97ac6ce6028
Ayan Das <ayan11112000@gmail.com>
2026-09-15 04:40:01 +0000

    Swap Vercel Analytics for the Cloudflare Web Analytics beacon

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MotionConfig } from 'motion/react'
-import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.tsx'
-
-inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
